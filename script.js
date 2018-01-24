a10a6d1ed728d8eafc7a773041d29f75894c5a4d
giotome <gio_tome@hotmail.com>
2018-01-23 22:52:06 -0200

    ajustes ema aulas

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let titulo = document.querySelector('h1');
 let texto = document.querySelector('p');
+let foto = document.querySelector('h2');
+let oi = document.querySelector('h3');
+let tudobem = document.querySelector('h4');
 
 function mudarTitulo() {
   titulo.innerHTML = 'Sua cidade';
@@ -7,4 +10,21 @@ function mudarTitulo() {
 
 function mudarTexto() {
   texto.style.color = 'red';
+  texto.style.backgroundColor = 'blue';
+}
+
+function mudarFoto() {
+  foto.style.color = 'pink';
+}
+
+function mudarOi() {
+  oi.innerHTML = 'Parabéns';
+  oi.style.color = '#e8f442';
+}
+
+function mudarTudobem() {
+  tudobem.style.color = '#26b8d6';
+  tudobem.style.backgroundColor = '#9226d6';
+
+
 }
